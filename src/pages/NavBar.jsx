8976879ce67a61f2a5772e5753045a5d7a9628de
Pau Serrano <pauserrano14@gmail.com>
2025-12-16 23:51:28 +0100

    modify Navbar CSS

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,16 +1,18 @@
 import { NavLink } from "react-router-dom"
 import "./NavBar.css"
 
-function NavBar () {
+function NavBar() {
     return (
         <nav className="navbar">
             <h3 className="logo">React Music App</h3>
             <div className="menu">
-                <NavLink  to = "/">Home</NavLink>
-        <NavLink to = "/newalbum">Add your Album</NavLink>
-        <NavLink to = "/acercade">About us</NavLink>
+                <ul>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/newalbum">Add your Album</NavLink></li>
+                <li><NavLink to="/acercade">About us</NavLink></li>
+                </ul>
             </div>
-        
+
         </nav>
     )
 }
