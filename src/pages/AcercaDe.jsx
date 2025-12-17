01813626650fc30990a823dca6621b283c128815
Jorge Jiménez <gexgt90@gmail.com>
2025-12-17 11:54:30 +0100

    add footer and page description

--- src/pages/AcercaDe.jsx
+++ src/pages/AcercaDe.jsx
@@ -1,10 +1,13 @@
 
 function AcerdaDe() {
     return (
-        <>
-        <h1>Acerca de</h1>
+        <div>
+        <p>Albumory es una aplicación diseñada para los amantes de la música que desean organizar, descubrir y conservar sus álbumes favoritos en un solo lugar. La plataforma permite a los usuarios crear una biblioteca musical personalizada, añadir álbumes de forma sencilla y mantener un registro claro de sus preferencias a lo largo del tiempo.
+
+Con una interfaz intuitiva y un enfoque centrado en la experiencia del usuario, Albumory facilita la exploración de colecciones personales, el orden por artistas, géneros o fechas, y la construcción de un archivo musical que refleja la identidad de cada oyente. Ideal tanto para oyentes casuales como para melómanos exigentes, Albumory transforma la forma de gestionar y disfrutar la música, poniendo cada álbum favorito al alcance de la mano.
+</p>
         <h3>WebDevs: Jorge Jiménez Morgado, Pau Serrano Herráiz</h3>
-        </>
+        </div>
     )
 }
 export default AcerdaDe
